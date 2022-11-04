6f68a5c4a203bfeb030aedcc68bb550268367a3a
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-11-04 17:02:29 +0100

    prestito

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 // See https://aka.ms/new-console-template for more information
 
 
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+
 Console.WriteLine("Buongiorno bibliotecario");
 Console.WriteLine("vuoi cercare un libro? inserisci il codice o il titolo");
 
@@ -21,8 +24,7 @@ foreach (Book book in documentList)
     }
 }
 
-Console.WriteLine("l'utente vuole effettuare un prestito?");
-
+//Parte user
 
 User user1 = new User("giuseppe", "acito", "email@mgm.it", "543336546554");
 
@@ -30,10 +32,27 @@ List<User> userList = new List<User>();
 
 userList.Add(user1);
 
-foreach (User user in userList)
+Console.WriteLine("l'utente vuole effettuare un prestito?");
+string prestito = Console.ReadLine();
+
+if (prestito == "si")
 {
+    Console.WriteLine("chi utente vuole effettuarlo?");
+    string userPrestito = Console.ReadLine();
+    foreach (User user in userList)
+    {
+        if (user.Name == userPrestito)
+        {
+            Console.WriteLine("utente registrato può fare il prestito");
+        }
+    }
 
 }
+
+
+
+
+
 
 
 
